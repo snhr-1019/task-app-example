98bc1e06f4fe6c1a2adec6ca1d70c2ee4c767deb
SONE Hiroaki <sonehiro@gmail.com>
2023-07-18 00:12:26 +0900

    sample

--- backend/todo-app-infrastructure/src/main/java/com/example/todoapp/infrastructure/TodoRepositoryDb.java
+++ backend/todo-app-infrastructure/src/main/java/com/example/todoapp/infrastructure/TodoRepositoryDb.java
@@ -5,13 +5,42 @@ import com.example.todoapp.domain.repository.TodoRepository;
 import com.example.todoapp.domain.vo.Code;
 import com.example.todoapp.domain.vo.Status;
 import com.example.todoapp.domain.vo.Title;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.impl.DSL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
+
+import static gen.jooq.todoapp.Tables.TODO;
+import static org.jooq.SQLDialect.MYSQL;
 
 public class TodoRepositoryDb implements TodoRepository {
     @Override
     public List<TodoEntity> fetch() {
         return null;
+    }
+
+    public static void main(String[] args) {
+        String userName = "root";
+        String password = "root";
+        String url = "jdbc:mysql://localhost:3306/todoapp";
+
+        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+            DSLContext create = DSL.using(conn, MYSQL);
+            Result<Record> result = create.select().from(TODO).fetch();
+
+            for (Record r : result) {
+                System.out.println("Code: " + r.getValue(TODO.CODE));
+                System.out.println("Title: " + r.getValue(TODO.TITLE));
+                System.out.println("Status: " + r.getValue(TODO.STATUS));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
