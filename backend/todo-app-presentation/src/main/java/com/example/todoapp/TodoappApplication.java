7b99ecf4d8a909b1b67e96e7faab65fbd4163d83
SONE Hiroaki <sonehiro@gmail.com>
2023-07-19 19:45:18 +0900

    format

--- backend/todo-app-presentation/src/main/java/com/example/todoapp/TodoappApplication.java
+++ backend/todo-app-presentation/src/main/java/com/example/todoapp/TodoappApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappApplication.class, args);
+    }
 
 }
