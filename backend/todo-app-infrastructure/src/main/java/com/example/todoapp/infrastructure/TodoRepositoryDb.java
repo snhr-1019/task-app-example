eeb720c645489b8e78a0c885bd2f4a5e2c164384
SONE Hiroaki <sonehiro@gmail.com>
2023-07-22 23:56:50 +0900

    dsl object injection

--- backend/todo-app-infrastructure/src/main/java/com/example/todoapp/infrastructure/TodoRepositoryDb.java
+++ backend/todo-app-infrastructure/src/main/java/com/example/todoapp/infrastructure/TodoRepositoryDb.java
@@ -5,6 +5,7 @@ import com.example.todoapp.domain.repository.TodoRepository;
 import com.example.todoapp.domain.vo.Code;
 import com.example.todoapp.domain.vo.Status;
 import com.example.todoapp.domain.vo.Title;
+import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
@@ -19,32 +20,27 @@ import static gen.jooq.todoapp.Tables.TODO;
 import static org.jooq.SQLDialect.MYSQL;
 
 @Repository
+@RequiredArgsConstructor
 public class TodoRepositoryDb implements TodoRepository {
+
+    private final DSLContext dsl;
+
     @Override
     public List<TodoEntity> fetch() {
-        String userName = "root";
-        String password = "root";
-        String url = "jdbc:mysql://localhost:3306/todoapp";
+        Result<Record> result = dsl.select().from(TODO).fetch();
 
-        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
-            DSLContext create = DSL.using(conn, MYSQL);
-            Result<Record> result = create.select().from(TODO).fetch();
-
-            for (Record r : result) {
-                System.out.println("Code: " + r.getValue(TODO.CODE));
-                System.out.println("Title: " + r.getValue(TODO.TITLE));
-                System.out.println("Status: " + r.getValue(TODO.STATUS));
-            }
-
-            var todo = new TodoEntity(
-                    new Code("ABC123"),
-                    new Title("牛乳を買う"),
-                    Status.TODO
-            );
-            return List.of(todo);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        for (Record r : result) {
+            System.out.println("Code: " + r.getValue(TODO.CODE));
+            System.out.println("Title: " + r.getValue(TODO.TITLE));
+            System.out.println("Status: " + r.getValue(TODO.STATUS));
         }
+
+        var todo = new TodoEntity(
+                new Code("ABC123"),
+                new Title("牛乳を買う"),
+                Status.TODO
+        );
+        return List.of(todo);
     }
 
     @Override
--- backend/todo-app-presentation/src/test/java/com/example/todoapp/controller/TodoControllerTest.java
+++ backend/todo-app-presentation/src/test/java/com/example/todoapp/controller/TodoControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,7 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(TodoController.class)
+@SpringBootTest
 @AutoConfigureMockMvc
 @Import(TodoAppPresentationConfig.class)
 public class TodoControllerTest {
