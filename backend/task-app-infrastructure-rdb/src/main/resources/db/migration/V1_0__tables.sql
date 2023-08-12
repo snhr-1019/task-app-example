7b61f61af1695dc8ee452dc9cfaa9d88e5ebeac9
SONE Hiroaki <sonehiro@gmail.com>
2023-08-12 23:40:51 +0900

    simplify task table

--- backend/task-app-infrastructure-rdb/src/main/resources/db/migration/V1_0__tables.sql
+++ backend/task-app-infrastructure-rdb/src/main/resources/db/migration/V1_0__tables.sql
@@ -1,29 +1,12 @@
-create table taskapp.app_user
+create table taskapp.tasks
 (
-    uuid     varchar(255) not null
+    id     integer      not null
         primary key,
-    username varchar(32)  not null,
-    password varchar(255) null,
-    constraint app_user_pk
-        unique (uuid)
-)
-    charset = utf8mb4;
-
-create table taskapp.task
-(
-    uuid          varchar(255) not null
-        primary key,
-    app_user_uuid varchar(255) not null,
-    code          varchar(255) not null,
-    title         varchar(255) not null,
-    status        varchar(255) not null
+    title  varchar(255) not null,
+    status varchar(255) not null
 );
 
-create index task_app_user_uuid_fk
-    on taskapp.task (app_user_uuid);
-
-
-INSERT INTO taskapp.task (uuid, app_user_uuid, code, title, status)
-VALUES ('9DA58AE2-96C4-44D0-9FC6-FBBB757C0F76', '74B2DDEB-5513-4820-82F5-F40BB41251E2', 'ABC123', '牛乳を買う', 'TASK');
-INSERT INTO taskapp.task (uuid, app_user_uuid, code, title, status)
-VALUES ('B92C0397-70D4-4F19-A98B-73B14C498EDF', '74B2DDEB-5513-4820-82F5-F40BB41251E2', '789EFG', '掃除をする', 'DOING');
+INSERT INTO taskapp.tasks (id, title, status)
+VALUES (1, '牛乳を買う', 'TODO');
+INSERT INTO taskapp.tasks (id, title, status)
+VALUES (2, '掃除をする', 'DOING');
