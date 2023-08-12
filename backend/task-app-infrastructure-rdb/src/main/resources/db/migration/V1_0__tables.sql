7e983e216175157fbd89f90c7fb9ac79802711c1
SONE Hiroaki <sonehiro@gmail.com>
2023-08-12 23:49:08 +0900

    edit tasks table

--- backend/task-app-infrastructure-rdb/src/main/resources/db/migration/V1_0__tables.sql
+++ backend/task-app-infrastructure-rdb/src/main/resources/db/migration/V1_0__tables.sql
@@ -1,12 +1,14 @@
 create table taskapp.tasks
 (
-    id     integer      not null
+    id        integer      not null
         primary key,
-    title  varchar(255) not null,
-    status varchar(255) not null
+    title     varchar(255) not null,
+    completed boolean      not null
 );
 
-INSERT INTO taskapp.tasks (id, title, status)
-VALUES (1, '牛乳を買う', 'TODO');
-INSERT INTO taskapp.tasks (id, title, status)
-VALUES (2, '掃除をする', 'DOING');
+INSERT INTO taskapp.tasks (id, title, completed)
+VALUES (1, '牛乳を買う', false);
+INSERT INTO taskapp.tasks (id, title, completed)
+VALUES (2, '掃除をする', false);
+INSERT INTO taskapp.tasks (id, title, completed)
+VALUES (3, '手紙を書く', true);
