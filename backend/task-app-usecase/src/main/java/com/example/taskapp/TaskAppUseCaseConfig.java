a78d32a9de4f6fd420fc60142ac0188b5a05d71f
SONE Hiroaki <sonehiro@gmail.com>
2023-08-12 23:03:59 +0900

    rename

--- backend/task-app-usecase/src/main/java/com/example/taskapp/TaskAppUseCaseConfig.java
+++ backend/task-app-usecase/src/main/java/com/example/taskapp/TaskAppUseCaseConfig.java
@@ -2,7 +2,6 @@ package com.example.taskapp;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan
--- backend/task-app-usecase/src/main/java/com/example/taskapp/usecase/ListTaskUseCase.java
+++ backend/task-app-usecase/src/main/java/com/example/taskapp/usecase/ListTaskUseCase.java
@@ -2,9 +2,7 @@ package com.example.taskapp.usecase;
 
 import com.example.taskapp.domain.entity.TaskEntity;
 import com.example.taskapp.domain.repository.TaskRepository;
-import com.example.taskapp.domain.vo.Code;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
