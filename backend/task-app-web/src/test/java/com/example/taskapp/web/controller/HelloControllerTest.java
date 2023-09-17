ba7907f609bdaf162f3beba9ce25a458d85609ff
SONE Hiroaki <sonehiro@gmail.com>
2023-09-17 23:27:21 +0900

    add HelloControllerTest

--- backend/task-app-web/src/test/java/com/example/taskapp/web/controller/HelloControllerTest.java
+++ backend/task-app-web/src/test/java/com/example/taskapp/web/controller/HelloControllerTest.java
@@ -11,38 +11,36 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = TaskappApplication.class)
 @AutoConfigureMockMvc
-public class TaskControllerTest {
+public class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Nested
-    class GetTaskApi {
+    class HelloApi {
         @Test
-        void testReturns401ResponseWithAnonymousUser() throws Exception {
+        void testReturns200ResponseWithAnonymousUser() throws Exception {
             mockMvc.perform(
-                            get("/task")
+                            get("/hello")
                                     .with(anonymous())
                                     .accept(MediaType.APPLICATION_JSON)
                     )
-                    .andExpect(status().isUnauthorized())
+                    .andExpect(status().isOk())
             ;
         }
 
         @Test
         void testReturns200ResponseWithAuthenticatedUser() throws Exception {
             mockMvc.perform(
-                            get("/task")
+                            get("/hello")
                                     .with(user("some_user").roles("ANY_ROLE"))
                                     .accept(MediaType.APPLICATION_JSON)
                     )
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.tasks[0].id").value("1"))
             ;
         }
     }
