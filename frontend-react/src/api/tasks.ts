13d21a8058c152e62eaa8ce483b83dbe9c35870f
SONE Hiroaki <sonehiro@gmail.com>
2024-01-03 23:34:00 +0900

    initial task list

---
+++ frontend-react/src/api/tasks.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {Task} from '../types/Task';
+
+const API_BASE_URL = 'http://localhost:8080/api';
+
+type GetTasksResponse = {
+    tasks: Task[];
+};
+
+export const getTasks = async (): Promise<GetTasksResponse> => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/task`);
+        return response.data;
+    } catch (error) {
+        console.error('Error while fetching tasks:', error);
+        throw error;
+    }
+};
