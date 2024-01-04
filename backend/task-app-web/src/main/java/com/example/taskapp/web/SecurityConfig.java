aea134c363587ba19295eeb87d3ff2fececd87e6
SONE Hiroaki <sonehiro@gmail.com>
2024-01-04 23:08:46 +0900

    consider cors

--- backend/task-app-web/src/main/java/com/example/taskapp/web/SecurityConfig.java
+++ backend/task-app-web/src/main/java/com/example/taskapp/web/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.example.taskapp.web.security.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +16,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         var authenticationManager = authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
         http
+                .cors(Customizer.withDefaults()) // CORSの無効化
                 .csrf(AbstractHttpConfigurer::disable) // CSRFトークンの無効化
                 .sessionManagement(session -> session // RESTなのでセッションをステートレスにする
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -46,6 +53,16 @@ public class SecurityConfig {
                         .authenticationEntryPoint(new JwtAuthenticationEntryPoint()) // 権限エラー時のハンドラー設定
                 );
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
