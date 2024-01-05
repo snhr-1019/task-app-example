70e4937d4f7eb3efb66bad07ab991e2456c39023
SONE Hiroaki <sonehiro@gmail.com>
2024-01-05 14:56:55 +0900

    create TaskList component

--- frontend-react/src/components/App.tsx
+++ frontend-react/src/components/App.tsx
@@ -1,34 +1,9 @@
-import {useEffect, useState} from 'react';
-import {getTasks} from '../api/tasks';
-//import './App.css';
-import {Task} from '../types/Task';
+import TaskList from "./TaskList";
 
 const App = () => {
-    // タスク一覧の初期値を空の配列とする
-    const [tasks, setTasks] = useState<Task[]>([]);
-
-    // コンポーネント起動時に全タスクを取得する
-    useEffect(() => {
-        const fetchTasks = async () => {
-            try {
-                const getTasksResponse = await getTasks();
-                setTasks(getTasksResponse.tasks);
-            } catch (error) {
-                console.error('Error while fetching tasks:', error);
-            }
-        };
-
-        fetchTasks();
-    }, []);
-
     return (
-        <div className="container">
-            <h1>ToDo List</h1>
-            <ul>
-                {tasks.map((task: Task) => (
-                    <li key={task.id}>{task.title}</li>
-                ))}
-            </ul>
+        <div className="App">
+            <TaskList />
         </div>
     );
 };
---
+++ frontend-react/src/components/TaskList.tsx
@@ -0,0 +1,36 @@
+import {useEffect, useState} from 'react';
+import {getTasks} from '../api/tasks';
+//import './TaskList.css';
+import {Task} from '../types/Task';
+
+const TaskList = () => {
+    // タスク一覧の初期値を空の配列とする
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    // コンポーネント起動時に全タスクを取得する
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try {
+                const getTasksResponse = await getTasks();
+                setTasks(getTasksResponse.tasks);
+            } catch (error) {
+                console.error('Error while fetching tasks:', error);
+            }
+        };
+
+        fetchTasks();
+    }, []);
+
+    return (
+        <div className="container">
+            <h1>ToDo List</h1>
+            <ul>
+                {tasks.map((task: Task) => (
+                    <li key={task.id}>{task.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TaskList;
